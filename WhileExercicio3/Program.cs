95163794bb9a9cff5469e2411f47a258dfd4c613
Thiago de Souza <thiagodesouzarb@gmail.com>
2023-11-22 20:20:49 -0300

    Exercicio posto de gasolina com while

---
+++ WhileExercicio3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileExercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0; 
+            Console.WriteLine("BEM VINDO AO POSTO DE GASOLINA BEBERRÃO!");
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Álcool");
+            Console.WriteLine("2 - Gasolina");
+            Console.WriteLine("3 - Diesel");
+            Console.WriteLine("4 - Fim");
+            Console.Write("Digite o numero correspondente do seu abastecimento: ");
+            int tipo = int.Parse(Console.ReadLine());
+
+            while (tipo != 4)
+            {
+                if (tipo == 1)
+                {
+                    alcool = alcool + 1;
+                }
+                else if (tipo == 2)
+                {
+                    gasolina = gasolina + 1;
+                }
+                else if (tipo == 3)
+                {
+                    diesel = diesel + 1;
+                }
+                Console.Write("Digite o numero correspondente do seu abastecimento: ");
+                tipo = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine($"Álcool: {alcool}");
+            Console.WriteLine($"Gasolina: {gasolina}");
+            Console.WriteLine($"Diesel: {diesel}");
+        }
+    }
+}
